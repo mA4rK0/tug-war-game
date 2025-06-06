e5a1250f9d439d6810d0f6671ad6fa0c32c1ce37
marko <onlymarko25@gmail.com>
2025-06-06 16:10:32 +0700

    feat: new smart contract address

--- src/contracts.ts
+++ src/contracts.ts
@@ -1,121 +1,120 @@
 export const wagmiContractConfig = {
-    // address: '0xB6F5675C778B9F3978862bDA5A97fB5c4757B946',
-    address: '0x205534484aa0529e4786966127eb0cf12e1a1a92',
-    abi: [
+  address: "0xa435Db16262e61d3d398aD8A231C4903C790cd2e",
+  abi: [
+    {
+      type: "constructor",
+      inputs: [
         {
-            "type": "constructor",
-            "inputs": [
-                {
-                    "name": "_owner",
-                    "type": "address",
-                    "internalType": "address"
-                }
-            ],
-            "stateMutability": "nonpayable"
+          name: "_owner",
+          type: "address",
+          internalType: "address",
         },
+      ],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "function",
+      name: "getWinStatus",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "getWinStatus",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "uint8",
-                    "internalType": "uint8"
-                }
-            ],
-            "stateMutability": "view"
+          name: "",
+          type: "uint8",
+          internalType: "uint8",
         },
+      ],
+      stateMutability: "view",
+    },
+    {
+      type: "function",
+      name: "maxScoreDifference",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "maxScoreDifference",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "uint8",
-                    "internalType": "uint8"
-                }
-            ],
-            "stateMutability": "view"
+          name: "",
+          type: "uint8",
+          internalType: "uint8",
         },
+      ],
+      stateMutability: "view",
+    },
+    {
+      type: "function",
+      name: "owner",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "owner",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "address",
-                    "internalType": "address"
-                }
-            ],
-            "stateMutability": "view"
+          name: "",
+          type: "address",
+          internalType: "address",
         },
+      ],
+      stateMutability: "view",
+    },
+    {
+      type: "function",
+      name: "pull",
+      inputs: [
         {
-            "type": "function",
-            "name": "pull",
-            "inputs": [
-                {
-                    "name": "isTeam1",
-                    "type": "bool",
-                    "internalType": "bool"
-                }
-            ],
-            "outputs": [],
-            "stateMutability": "nonpayable"
+          name: "isTeam1",
+          type: "bool",
+          internalType: "bool",
         },
+      ],
+      outputs: [],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "function",
+      name: "reSet",
+      inputs: [
         {
-            "type": "function",
-            "name": "reSet",
-            "inputs": [
-                {
-                    "name": "_maxScoreDifference",
-                    "type": "uint8",
-                    "internalType": "uint8"
-                }
-            ],
-            "outputs": [],
-            "stateMutability": "nonpayable"
+          name: "_maxScoreDifference",
+          type: "uint8",
+          internalType: "uint8",
         },
+      ],
+      outputs: [],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "function",
+      name: "ropePosition",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "ropePosition",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "int8",
-                    "internalType": "int8"
-                }
-            ],
-            "stateMutability": "view"
+          name: "",
+          type: "int8",
+          internalType: "int8",
         },
+      ],
+      stateMutability: "view",
+    },
+    {
+      type: "function",
+      name: "team1Score",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "team1Score",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "uint8",
-                    "internalType": "uint8"
-                }
-            ],
-            "stateMutability": "view"
+          name: "",
+          type: "uint8",
+          internalType: "uint8",
         },
+      ],
+      stateMutability: "view",
+    },
+    {
+      type: "function",
+      name: "team2Score",
+      inputs: [],
+      outputs: [
         {
-            "type": "function",
-            "name": "team2Score",
-            "inputs": [],
-            "outputs": [
-                {
-                    "name": "",
-                    "type": "uint8",
-                    "internalType": "uint8"
-                }
-            ],
-            "stateMutability": "view"
-        }
-    ]
-} as const
+          name: "",
+          type: "uint8",
+          internalType: "uint8",
+        },
+      ],
+      stateMutability: "view",
+    },
+  ],
+} as const;
